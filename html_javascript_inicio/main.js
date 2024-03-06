038232607485e45dc40a3e86002d6069ee23d3e2
RyZe177 <miguelinho123pro@gmail.com>
2024-03-06 12:35:55 -0100

    js

--- html_javascript_inicio/main.js
+++ html_javascript_inicio/main.js
@@ -11,12 +11,23 @@ $(document).ready(function(){ // ready e uma função que aceita outra função
         //mostrar detalhes do evento na consolaea wqWEJ«I-
         console.log(event);
     });
+    
     //quando o fomulario for submetido
+    $("#loader").hide(); //escoder o loader (linha 57 html)
     $("form").on("submit",function(event){
         //log q o evento aconteceu
         console.log("submit");
         //prevenir açao default = que a pagina seja recarregada
         event.preventDefault();
+        //mostrar loader
+        $("#loader").fadeIn(1000, function(){
+            console.log ("fadeIn completo");
+            $(this).find("span").text("Registo Efetuado");
+            //funcionamento do botao fechar
+            $(this).find('.close').on ("click",function(){
+                $("#loader").fadeOut(1000);
+            });
+        });
     });
 
 });
