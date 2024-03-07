d5fdde8a2565e717981a15180b6eb12598724cdf
RyZe177 <miguelinho123pro@gmail.com>
2024-03-07 12:38:45 -0100

    js

---
+++ accordion/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //gravar seletores em variaveis
+    var question = $ ("dl");
+    var title = $ ("dt");
+    var description = $ ("dd");
+
+    description.hide();
+    question.on ("click", function(){ //quando eu clico abre a funçao
+        var thisQuestion = $(this);
+        //abrir ou fechar a pergunta selecionada
+        if(thisQuestion.is(".open")) {  //se ja tiver aberta:
+            thisQuestion.removeClass("open").find("dd").slideUp();
+        } else { //caso contrario (se nao tiver aberta):
+            thisQuestion.addClass("open").find("dd").slideDown(); //adiciona a classe open ao dd e desliza-o pa baixo
+        }
+        //fechar outras perguntas abertas
+        if(thisQuestion.siblings(".open")){ //this=dl || siblings=tudo oq esta ao mm nivel de tabelaçao 
+            thisQuestion.siblings(".open").find("dd").slideUp(function(){ // pega no sibling com a classe open e fecha a sua descriçao
+                $(this).closest(".open").removeClass("open"); // quando o slideUp acabar tira a classe open do elemento mais prox com essa classe
+            });
+        }
+    }); 
+});
