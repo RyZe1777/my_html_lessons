8f98ea4b468311b191b8e1db5718400cc1aa8021
RyZe177 <miguelinho123pro@gmail.com>
2024-03-14 12:26:36 -0100

    js e notas

--- questionario/main.js
+++ questionario/main.js
@@ -12,7 +12,26 @@ $(document).ready(function(){
             "quest3": "Pêra",
             "quest4": "KFC",
             "quest5": "C++",
-            
         };
+        var correct = 0;
+        var wrong = 0;  
+        answers.each(function(){
+            var thisV = $(this);
+            var name = thisV[0]["name"];
+            var id = thisV[0]["id"];
+            console.log(name+ " - "+id);
+            if(matrix[name] == id){
+                console.log("CORRETO");
+                correct++;
+                thisV.addClass("correct");
+            }   else {
+                console.log("ERRADO");
+                wrong++;
+                thisV.addClass("wrong");
+            }
+        });
+        $("form").append("Tem" +correct+"perguntas certas"
+        + " e "+wrong+"perguntas erradas");
+
     });
 });
